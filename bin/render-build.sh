a861299642527ab2bd47878ab51ea43905b51816
murakamihibiki <rstoyo.hibiki.murakami@gmail.com>
2023-09-08 15:54:06 +0900

    Update render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,1 +1,8 @@
-bundle install; bundle exec rake assets:precompile; bundle exec rake assets:clean;
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
